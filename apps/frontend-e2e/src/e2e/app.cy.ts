ec05913f05b6b8a70ad34e256a38af0562fe6c2f
Kriti Seth <kriti.seth@joborce.com>
2024-10-23 18:24:54 +0530

    fix design

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('frontend-e2e', () => {
         cy.login('my-email@something.com');
 
         // Function helper example, see `../support/app.po.ts` file
-        getGreeting().contains(/Link Your World with/);
+        getGreeting().contains(/Link Your World With/);
     });
 });
